c9db13c1b171d14a9ba15e9b06d46b67509603da
Dipesh437 <dipeshkarki437@gmail.com>
2024-11-27 20:04:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    alert('Thank you for your message, Dipesh will get back to you soon!');
+    this.reset();
+  });
+  
